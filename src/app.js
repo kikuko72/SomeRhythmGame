38db7e07395daebf339b6a2a05d40efca8da0cce
kikuko72 <6210037@gmail.com>
2018-01-06 22:53:08 +0900

    ノーツのプレースホルダーの表示まで実装

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const difficulty = {
 }
 
 const noteType = {
-      none   : { label : 'なし'         , value: 0}
-    , single : { label : '単独ノーツ'    , value: 1}
-    , large  : { label : '単独ノーツ(大)', value: 2}
-    , right  : { label : '右フリック'    , value: 3}
-    , left   : { label : '左フリック'    , value: 4}
-    , up     : { label : '上フリック'    , value: 5}
-    , linked : { label : '連結ノーツ'    , value: 6}
+      placeholder   : { label : 'なし'         , value: 0, class: 'placeholder'  }
+    , single : { label : '単独ノーツ'    , value: 1, class: 'normal'}
+    , large  : { label : '単独ノーツ(大)', value: 2, class: 'large' }
+    , right  : { label : '右フリック'    , value: 3, class: 'right' }
+    , left   : { label : '左フリック'    , value: 4, class: 'left'  }
+    , up     : { label : '上フリック'    , value: 5, class: 'up'    }
+    , linked : { label : '連結ノーツ'    , value: 6, class: 'normal'}
 }
 
 const noteTypes = Object.keys(noteType).map(key => noteType[key]);
@@ -23,7 +23,7 @@ const initNotes = (columns, lines) => {
         notes[columnIndex] = [];
         for (let lineIndex = 0; lineIndex < lines; lineIndex++) {
             notes[columnIndex][lineIndex] = {
-                type : noteType.none
+                type : noteType.placeholder
                 , x : columnIndex
                 , y : lineIndex
             }
@@ -32,8 +32,14 @@ const initNotes = (columns, lines) => {
     return notes;
 }
 
+const difficultyState = difficulty.MM;
+const initialLines = 8;
+const basicNoteDiameter = 32
+
 const state = {
-  notes: initNotes(difficulty.MM.columns)
+    difficulty: difficultyState
+  , lines: initialLines
+  , notes: initNotes(difficultyState.columns, initialLines)
   , editType: noteType.single
 }
 
@@ -44,15 +50,20 @@ const actions = {
     }
 }
 
+const calculateCX = x => basicNoteDiameter * (x * 2 + 1);
+const calculateCY = y => basicNoteDiameter * (y + 1);
+const calculateSvgWith = () => calculateCX(state.difficulty.columns);
+const calculateSvgHeight = () => calculateCY(state.lines);
+
 const view = (state, actions) => (
   <div>
-    <svg id="score" width="208px" height="288px" viewBox="0 0 208 288"
+    <svg id="score" width={calculateSvgWith()} height={calculateSvgHeight()} viewBox={'0 0 ' + calculateSvgWith() + ' ' + calculateSvgHeight()}
          xmlns="http://www.w3.org/2000/svg" version="1.1">
+         {state.notes.map(columns => columns.map(point => <circle cx={calculateCX(point.x)} cy={calculateCY(point.y)} r={basicNoteDiameter / 2} class={point.type.class}/>))}
     </svg>
     <select id="editType" onchange={actions.changeEditType} value={state.editType.value}>
       {noteTypes.map(note => <option value={note.value}>{note.label}</option>)}
     </select>
-    <p>{state.editType.label}</p>
   </div>
 )
 
